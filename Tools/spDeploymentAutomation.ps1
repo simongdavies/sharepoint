549126a089ee1e27a21161574dfcf0d9be2ed401
kenazk <kenazk@microsoft.com>
2015-09-04 09:49:17 -0700

    Added LCM settings to PDC

--- Tools/spDeploymentAutomation.ps1
+++ Tools/spDeploymentAutomation.ps1
@@ -1,14 +1,14 @@
 ﻿Switch-AzureMode -name AzureResourceManager
 
 # Count of runs
-$count = 3
+$count = 1
 
 # Variables
 $templateFile = "C:\Users\kenazk\Desktop\GitHub\sharepoint\dsc2.1\mainTemplate-serialized-dsc24.json"
 $paramsFile = "C:\Users\kenazk\Desktop\GitHub\sharepoint\parameters.json"
 $params = Get-content $paramsFile | convertfrom-json
 $location = "westus"
-$rgprefix = "T33p"
+$rgprefix = "T34p"
 $premium = $true
 
 # Generate parameter object
