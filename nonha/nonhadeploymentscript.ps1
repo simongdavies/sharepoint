9a64b421f5c623fd777847e2a7ab8e76cecf31b1
kenazk <kenazk@microsoft.com>
2015-09-09 10:37:39 -0700

    Upgrading to 2.4

---
+++ nonha/nonhadeploymentscript.ps1
@@ -0,0 +1,70 @@
+﻿Switch-AzureMode -name AzureResourceManager
+
+#######################################
+###       NON HA SHAREPOINT         ###
+#######################################
+
+# Count of runs
+$count = 10
+
+# Variables
+$templateFile = "C:\Users\kenazk\Desktop\GitHub\sharepoint\nonha\mainTemplate.json"
+$paramsFile = "C:\Users\kenazk\Desktop\GitHub\sharepoint\nonha\parameters.json"
+$params = Get-content $paramsFile | convertfrom-json
+$location = "westus"
+$rgprefix = "T51s"
+$premium = $false
+
+# Generate parameter object
+$hash = @{};
+foreach($param in $params.psobject.Properties)
+{
+    $hash.Add($param.Name, $param.Value.Value);
+}
+
+#Create new Resource Groups and Deployments for each run
+for($i = 0; $i -lt $count; $i++)
+{
+    # Create new Resource Group
+    $d = get-date
+    $rgname = $rgprefix + '-'+ $d.Year + $d.Month + $d.Day + '-' + $d.Hour + $d.Minute + $d.Second
+    New-AzureResourceGroup -Name $rgname -Location $location -Verbose 
+    
+    # Construct parameter set
+    $dsuffix = "" + $d.hour + $d.minute + $d.Second
+    $hash.spDnsPrefix = "spnonha" + $dsuffix
+    $hash.storageAccountName = "sa" + $dsuffix
+    $hash.location = $location
+
+    if ($premium)
+    {
+        $hash.StorageAccountType = "Premium_LRS"
+        $hash.spVMSize = "Standard_DS3"
+        $hash.sqlVMSize = "Standard_DS3"
+        $hash.adVMSize = "Standard_DS1"
+    } 
+    else
+    {
+        $hash.StorageAccountType = "Standard_LRS"
+        $hash.spVMSize = "Standard_D3"
+        $hash.sqlVMSize = "Standard_D3"
+        $hash.adVMSize = "Standard_D1"
+    }
+
+    # Run as asynchronous job
+    $jobName = "spDeployment-" + $i;
+    $sb = {
+        param($rgname, $templateFile, $hash)
+        function createSharepointDeployment($rgname, $templateFile, $hash)
+        {
+            $dep = $rgname + "-dep"; 
+            New-AzureResourceGroupDeployment -ResourceGroupName $rgname -Name $dep -TemplateFile $templateFile -TemplateParameterObject $hash -Verbose 
+        }
+        createSharepointDeployment $rgname $templateFile $hash
+    }
+    $job = start-job -Name $jobName -ScriptBlock $sb -ArgumentList $rgname, $templateFile, $hash
+
+    Start-sleep -s 5
+}
+
+
