660b80f74b31d3eca2adc9e893ac80377411d31a
kenazk <kenazk@microsoft.com>
2015-09-21 11:12:49 -0700

    Adding rdp setup template

--- Tools/spDeploymentAutomation.ps1
+++ Tools/spDeploymentAutomation.ps1
@@ -13,7 +13,7 @@ $templateFile = "C:\Users\kenazk\Desktop\GitHub\sharepoint\azurecon\mainTemplate
 $paramsFile = "C:\Users\kenazk\Desktop\GitHub\sharepoint\parameters.json"
 $params = Get-content $paramsFile | convertfrom-json
 $location = "westus"
-$rgprefix = "AC5"
+$rgprefix = "AC6"
 $premium = $true
 
 # Generate parameter object
